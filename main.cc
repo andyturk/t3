30a335510d94193e4aab06ef68648e24ee86b1e1
Andy Turk <andyturk@yahoo.com>
2012-06-02 10:52:36 -0700

    initialize via HCITransport

--- main.cc
+++ main.cc
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include "hal.h"
+#include "bluetooth.h"
 
 #include "utils/uartstdio.h"
 
@@ -7,30 +8,32 @@ IOPin pf3('F', 3, IOPin::LED);
 IOPin pc4('C', 4, IOPin::OUTPUT);
 UART0 uart0;
 UART1 uart1;
+HCITransport pan1323(uart1, pc4);
 
 extern "C" int main() {
   CPU::set_clock_rate_50MHz();
-  uart1.configure();
+  pan1323.configure();
+  //uart1.configure();
   uart0.configure();
   pf3.configure();
-  pc4.configure();
+  //pc4.configure();
 
-  pc4.set_value(0); // assert reset on pan 1323
+  //pc4.set_value(0); // assert reset on pan 1323
 
   UARTStdioInitExpClk(0, 115200); // UART0 is the console
   UARTprintf("console initialized\n");
 
-  uart1.initialize();
-  uart1.set_baud(115200);
-  uart1.enable(true);
+  //uart1.initialize();
+  //uart1.set_baud(115200);
+  //uart1.set_enable(true);
 
   const uint32_t msec = 150;
 
-  pc4.set_value(1); // de-assert reset on pan1323
+  //pc4.set_value(1); // de-assert reset on pan1323
 
-  pf3.set_value(1); // turn on LED
-  CPU::delay(150);
-  pf3.set_value(0); // turn off LED
+  //pf3.set_value(1); // turn on LED
+  //CPU::delay(150);
+  //pf3.set_value(0); // turn off LED
 
   uart1.put(0x01);
   uart1.put(0x03);
