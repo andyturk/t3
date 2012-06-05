b0b878237a9609cf9060cca945af4c251abdbfe6
Andy Turk <andyturk@yahoo.com>
2012-06-04 18:44:36 -0700

    handle interrupts

--- bluetooth.cc
+++ bluetooth.cc
@@ -1,22 +1,76 @@
 #include "bluetooth.h"
+#include "register_defs.h"
 
-HCITransport::HCITransport(UART &uart, IOPin &shutdown) :
-  uart(uart), shutdown(shutdown)
+UARTBuffer::UARTBuffer(UART &uart) :
+  uart(uart),
+  rx(rx_storage, sizeof(rx_storage)),
+  tx(tx_storage, sizeof(tx_storage))
 {
 }
 
-void HCITransport::configure() {
+void UARTBuffer::drain_rx_fifo() {
+  while (uart.can_read() && rx.write_capacity() > 0) {
+    rx.write1(uart.read1());
+  }
+}
+
+void UARTBuffer::fill_tx_fifo() {
+  while (uart.can_write() && tx.read_capacity() > 0) {
+    uint8_t byte;
+    tx.read1(byte);
+    uart.write1(byte);
+  }
+
+  // enable the tx interrupt if there's more data in the ringbuffer
+  uart.set_interrupt_sources(UART::RX | UART::ERROR | (tx.read_capacity() > 0 ? UART::TX : 0));
+}
+
+void UARTBuffer::interrupt_handler() {
+  uint32_t cause = uart.clear_interrupt_cause(UART::RX | UART::TX | UART::ERROR);
+
+  if (cause & UART::ERROR) {
+    error();
+  }
+
+  if (cause & UART::TX) {
+    fill_tx_fifo();
+  }
+
+  if (cause & UART::RX) {
+    drain_rx_fifo();
+    data_received();
+  }
+}
+
+size_t UARTBuffer::write(const uint8_t *buffer, size_t length) {
+  uart.set_interrupt_enable(false);
+  size_t count = tx.write(buffer, length);
+  fill_tx_fifo();
+  uart.set_interrupt_enable(true);
+  return count;
+}
+
+HCI::HCI(UART &uart, IOPin &shutdown) :
+  UARTBuffer(uart), shutdown(shutdown)
+{
+}
+
+void HCI::configure() {
   shutdown.configure();
   uart.configure();
 }
 
-void HCITransport::initialize() {
+void HCI::initialize() {
   shutdown.initialize();
   shutdown.set_value(0); // assert SHUTDOWN
   uart.initialize();
   uart.set_baud(115200);
   uart.set_enable(true);
+  uart.set_interrupt_enable(true);
 
   shutdown.set_value(1); // clear SHUTDOWN
   CPU::delay(150); // wait 150 msec
 }
+
+
+
--- bluetooth.h
+++ bluetooth.h
@@ -1,13 +1,114 @@
 #pragma once
 
+#include <cstddef>
 #include "hal.h"
 
-class HCITransport {
-  UART &uart;
-  IOPin &shutdown;
+template<class T>
+class RingBuffer {
+  T *const buffer;
+  const size_t capacity;
+  uint32_t read_position, write_position;
 
  public:
-  HCITransport(UART &uart, IOPin &shutdown);
+  RingBuffer(T *const buffer, size_t capacity) :
+    buffer(buffer), capacity(capacity), read_position(0), write_position(0) {}
+  
+  void flush() {
+    read_position = write_position = 0;
+  }
+
+  size_t read_capacity() const {
+    size_t cap = 0xffffffff;
+
+    if (write_position >= read_position) {
+      cap = write_position - read_position;
+    } else {
+      cap = (capacity - read_position) + write_position;
+    }
+
+    return cap;
+  }
+
+  size_t write_capacity() const {
+    if (write_position >= read_position) {
+      return (capacity - write_position) + read_position;
+    } else {
+      return read_position - write_position;
+    }
+  }
+
+  /*
+   * 
+   */
+  size_t read(T *dst, size_t n) {
+    size_t read_count = 0;
+
+    while (n > 0 && read_capacity() > 0) {
+      size_t run = (write_position >= read_position) ?
+        (write_position - read_position) : (capacity - read_position);
+      if (n < run) run = n;
+      for (size_t i=0; i < run; ++i) dst[i] = buffer[read_position + i];
+      read_position += run;
+      if (read_position == capacity) read_position = 0;
+      read_count += run;
+      n -= run;
+    };
+
+    return read_count;
+  }
+
+  inline bool read1(T &dst) {
+    return read(&dst, 1) == 1;
+  }
+
+  size_t write(const T *src, size_t n) {
+    size_t write_count = 0;
+
+    while (n > 0 && write_capacity() > 0) {
+      size_t run = (write_position >= read_position) ?
+        (capacity - write_position) : (read_position - write_position);
+      if (n < run) run = n;
+      for (size_t i=0; i < run; ++i) buffer[write_position + i] = src[i];
+      write_position += run;
+      if (write_position == capacity) write_position = 0;
+      write_count += run;
+      n -= run;
+    };
+
+    return write_count;
+  }
+
+  inline bool write1(const T &src) {
+    return write(&src, 1) == 1;
+  }
+};
+
+class UARTBuffer {
+  uint8_t rx_storage[1024];
+  uint8_t tx_storage[512];
+
+ protected:
+  void drain_rx_fifo();
+  void fill_tx_fifo();
+
+  UART &uart;
+  virtual void data_received() {}
+  virtual void error() {for(;;);}
+
+ public:
+  RingBuffer<uint8_t> rx;
+  RingBuffer<uint8_t> tx;
+
+  UARTBuffer(UART &uart);
+  size_t write(const uint8_t *buffer, size_t length);
+  void interrupt_handler();
+};
+
+class HCI : public UARTBuffer {
+  IOPin &shutdown;
+  
+ public:
+  HCI(UART &uart, IOPin &shutdown);
 
   void configure();
   void initialize();
