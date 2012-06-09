9663247da7ea8de2aef6f84633a45c25ff723809
Andy Turk <andyturk@yahoo.com>
2012-06-09 15:01:38 -0700

    moved configurations out of Baseband

--- main.cc
+++ main.cc
@@ -12,32 +12,24 @@ Baseband pan1323(uart1, pc4);
 
 extern "C" int main() {
   CPU::set_clock_rate_50MHz();
-
   CPU::set_master_interrupt_enable(false);
 
+  led1.configure();
+  pc4.configure();
   uart0.configure();
+  uart1.configure();
+
   UARTStdioInitExpClk(0, 115200); // UART0 is the console
   UARTprintf("console initialized\n");
 
-  led1.configure();
   led1.initialize();
   led1.set_value(0);
-
-  pan1323.configure();
+  
   pan1323.initialize();
 
   CPU::set_master_interrupt_enable(true);
 
   pan1323.send(HCI::RESET);
-
-  /*
-  while (uart1.rx.read_capacity() < 7) {
-    led1.set_value(1);
-    led1.set_value(0);
-  }
-
-  led1.set_value(1); // turn on LED
-  */
 
   for(;;);
 }
