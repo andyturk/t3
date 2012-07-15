e7f37d0fbbdb19583853e226f2e2b9bb7c214615
Andy Turk <andyturk@yahoo.com>
2012-07-15 14:32:34 -0700

    dump hex bytes correctly

--- debug.h
+++ debug.h
@@ -1,9 +1,15 @@
 #pragma once
 
 #ifdef DEBUG
-#include "utils/uartstdio.h"
+//#include "utils/uartstdio.h"
+#include "screen.h"
 
 inline void dump_hex_bytes(uint8_t *p, unsigned int len) {
-  while (len--) UARTprintf("%02x ", *p++);
+  if (len) printf("  ");
+
+  for (unsigned int i=0; i < len; ++i) {
+    printf("%02x ", *p++);
+    if (i > 15 && (0xf & i) == 0) printf("\n  ");
+  }
 }
 #endif
