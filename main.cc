999dac53dde78a35f3c397f4c36ef8ac496bf0c7
Andy Turk <andyturk@yahoo.com>
2012-07-15 14:54:21 -0700

    don't use uart0 any more

--- main.cc
+++ main.cc
@@ -7,11 +7,11 @@
 #include "h4.h"
 #include "att.h"
 #include "gatt.h"
-
+#include "screen.h"
 
 IOPin led1('F', 3, IOPin::LED);
 IOPin pc4('C', 4, IOPin::OUTPUT);
-UART0 uart0;
+//UART0 uart0;
 UART1 uart1;
 BBand pan1323(uart1, pc4);
 SysTick systick(1000);
@@ -41,15 +41,16 @@ MyService my;
 extern "C" int main() {
   CPU::set_clock_rate_50MHz();
   CPU::set_master_interrupt_enable(false);
+  Screen::the_screen.initialize();
 
   led1.configure();
   pc4.configure();
-  uart0.configure();
+  //uart0.configure();
   uart1.configure();
   systick.configure();
 
-  UARTStdioInitExpClk(0, 115200); // UART0 is the console
-  printf("console initialized\n");
+  //  UARTStdioInitExpClk(0, 115200); // UART0 is the console
+  // printf("console initialized\n");
 
   //systick.initialize();
 
@@ -69,6 +70,5 @@ extern "C" int main() {
 
 extern "C" void __attribute__ ((isr)) uart_1_handler() {
   h4.uart_interrupt();
-  //pan1323.uart_interrupt_handler();
 }
 
